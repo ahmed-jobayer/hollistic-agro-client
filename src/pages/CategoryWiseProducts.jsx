7dbe4c17054bb33e852f8f07f4a02b6a9ecbad3b
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-04-29 00:53:33 +0600

    conditionl rendering added to the category wise product

--- src/pages/CategoryWiseProducts.jsx
+++ src/pages/CategoryWiseProducts.jsx
@@ -15,7 +15,6 @@ const CategoryWiseProducts = () => {
     queryKey: ['products', category],
     queryFn: async()=> {
       const res = await axiosPubluic.get(`/products/${category}`)
-      console.log(res);
       if (!res?.statusText === "OK") {
         Swal.fire("Failed to load data")
       }
@@ -24,21 +23,22 @@ const CategoryWiseProducts = () => {
     staleTime: 60000 // 1 minute before refetching the same data
   })
 
-
-  console.log({isLoading, products});
-
+  
+  
   if (isLoading) {
     return <Loader />;
   }
-
+  const length  = products?.length
 
   return (
     <div className="min-h-screen">
 
-    <div className="container mx-auto p-3 grid gap-3 lg:grid-cols-5">
-      {products?.map((product) => (
+    <div className="container mx-auto p-3 grid gap-3 lg:grid-cols-5 ">
+      {length > 0 ? products?.map((product) => (
         <ProductCard handleAddToCart={handleAddToCart} product={product} key={product?._id} />
-      ))}
+      ))
+    : <p className=" min-h-screen lg:col-span-5  flex justify-center ">There is no products in this Category</p>
+    }
     </div>
     </div>
   );
